db3c98f4215a148208daa109c6fcccaf50454841
Stuart Lee <lee.s@wehi.edu.au>
2020-01-28 12:20:30 +1100

    rebuild paper on build branch

--- _render.R
+++ _render.R
@@ -1,7 +1,7 @@
 options(warn = 1)
 library(rmarkdown)
 
-paper <- "./vignettes/fluentGenomics.Rmd"
+paper <- here::here("vignettes", "fluentGenomics.Rmd")
 
 # spell check
 wordlist <- readLines("WORDLIST")
@@ -11,22 +11,24 @@ if (length(spell_res$word) > 0) {
   stop("Can you please fix typos listed above first?", call. = FALSE)
 }
 
+
 # compile as pdf to docs folder
 if (Sys.getenv("RSTUDIO") != "1" && Sys.info()['sysname'] == "Darwin") {
   Sys.setenv('RSTUDIO_PANDOC' = '/Applications/RStudio.app/Contents/MacOS/pandoc')
 }
 
-output_dir <- "./docs/"
 
 pdfdoc <- function(...) {
   bookdown::pdf_document2(toc = FALSE, 
                           highlight = "pygments",
                           fig_width = 5,
-                          keep_tex = TRUE)
+                          keep_tex = TRUE
+  )
 }
   
 
 render(paper,
        output_format = pdfdoc(),
-       output_dir = "./docs/",
+       output_dir = here::here("docs"),
        quiet = FALSE)
+
